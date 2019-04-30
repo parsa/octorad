9c13a72de3e99e17ed1fd2e6abff402f118d2b88
Parsa Amini <parsa@cct.lsu.edu>
2019-04-30 17:10:13 -0500

    cmake groundwork for vc support

--- configure.bat
+++ configure.bat
@@ -1,4 +1,4 @@
 rd /s /q cmake-build-debug
-cmake -H. -Bcmake-build-debug -G"Visual Studio 15 2017 Win64" -DOCTORAD_DUMP_DIR=C:/Users/Parsa/Desktop/arg-dumps
+cmake -H. -Bcmake-build-debug -G"Visual Studio 15 2017 Win64" -DOCTORAD_DUMP_DIR=C:/Users/Parsa/Desktop/arg-dumps -DOCTORAD_WITH_VC=OFF -DVc_DIR="C:/Repos/Vc/cmake-install-debug/lib/cmake/Vc"
 pause
 cmake --open cmake-build-debug
