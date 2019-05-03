8778ec89b87c031f71e4fc7ef302ecec14a44cea
Parsa Amini <parsa@cct.lsu.edu>
2019-05-03 17:58:03 -0500

    put kernel in a separate library

--- radiation/kernel_gpu.cu
+++ radiation/kernel_gpu.cu
@@ -302,7 +302,7 @@ __device__ void abort_if_solver_not_converged(double const eg_t0, double E0, F c
         }
     }
     // Error is not smaller that error tolerance after performed iterations. Abort.
-    std::printf("Implicit radiation solver failed to converge\n");
+    std::printf("implicit radiation solver failed to converge\n");
     // HACK: disabled since it prevented nvprof from showing device utilization info
     // assert(false);
 }
